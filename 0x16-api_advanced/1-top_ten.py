528c4033e92484f5e8456e4345f4dd5467cb39c4
ntando007 <111117959+Ntando007@users.noreply.github.com>
2023-05-09 15:53:12 +0200

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,7 +3,7 @@
 titles of the first 10 hot posts listed for a given subreddit. """
 
 import requests
-headers = {"User-Agent": "ubuntu:hbtn:v1.0 (by /u/piroli_)"}
+headers = {"User-Agent": "ubuntu:devops:v1.0 (by /u/ntando_)"}
 
 
 def top_ten(subreddit):
